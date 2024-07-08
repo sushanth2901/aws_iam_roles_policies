850fd10dde64b6fbc4eb873d622711b0bca7048e
sushanth2901 <75194546+sushanth2901@users.noreply.github.com>
2024-07-08 18:47:54 +0530

    Update create_role.py

--- create_role.py
+++ create_role.py
@@ -1,27 +1,24 @@
 import boto3
 import json
-import sys
 
-role_name = sys.argv[1]
+def create_policy(policy_name, policy_document):
+    client = boto3.client('iam')
+    
+    response = client.create_policy(
+        PolicyName=policy_name,
+        PolicyDocument=json.dumps(policy_document)
+    )
+    
+    print(f"Policy {policy_name} created successfully.")
+    print(response)
 
-client = boto3.client('iam')
+def create_policies_from_file(policies_file):
+    with open(policies_json.json, 'r') as file:
+        policies_data = json.load(file)
+    
+    # Iterate through the list of policies and create each one
+    for policy in policies_data['policies']:
+        create_policy(policy['PolicyName'], policy['PolicyDocument'])
 
-trust_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-
-response = client.create_role(
-    RoleName=role_name,
-    AssumeRolePolicyDocument=json.dumps(trust_policy)
-)
-
-print(response)
+# Example usage
+create_policies_from_file('policies.json')
